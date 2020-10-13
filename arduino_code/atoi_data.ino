b7e01902ddbb8aec924f5ef2fcefe3e6d73195bb
João Victor Palhares <36644804+vicpb@users.noreply.github.com>
2020-10-13 08:19:27 -0300

    Update atoi_data.ino

--- arduino_code/atoi_data.ino
+++ arduino_code/atoi_data.ino
@@ -16,12 +16,11 @@ char mover;
 char *_posicao;
  
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     attack.attach(9, 1, 180);
     elevator.attach(10, 1, 180);
     claw.attach(11, 1, 180);
     body.attach(8, 1, 180);
-
 }
 
 void loop() {
@@ -34,31 +33,40 @@ void loop() {
    posicao_attack = posicao_elevator = posicao_claw = posicao_body = atoi(_posicao);
   }
   
-  if (mover == 'q'){
+  if (mover == 'a'){
     attack.write(posicao_attack);
     Serial.println("movendo o objeto attack para o ângulo: ");
     Serial.println(int(posicao_attack));
-    mover = 'z';
+    mover = "0x00";
   }
   
-   else if (mover == 'w'){
+   else if (mover == 'b'){
     elevator.write(posicao_elevator);
     Serial.println("movendo o objeto elevator para o ângulo: ");
     Serial.println(int(posicao_elevator));
-    mover = 'z';
+    mover = "0x00";
   }
   
-  else if (mover == 'e'){
+  else if (mover == 'c'){
     claw.write(posicao_claw);
     Serial.println("movendo o objeto claw para o ângulo: ");
     Serial.println(int(posicao_claw));
-    mover = 'z';
+    mover = "0x00";
   }
 
-  else if (mover == 'r'){
+  else if (mover == 'd'){
     body.write(posicao_body);
     Serial.println("movendo o objeto body para o ângulo: ");
     Serial.println(int(posicao_body));
-    mover = 'z';
-  }  
+    mover = "0x00";
+  }
+
+  else if (mover == 'e'){
+    attack.write(90);
+    elevator.write(30);
+    claw.write(45);
+    body.write(90);
+    Serial.println("set: 0");
+    mover = "0x00";
+  }
 }
